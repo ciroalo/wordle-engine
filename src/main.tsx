d87726f7e913b06ebaa355c849bc902d6809bfec
ciroalo <ciroalo2000@gmail.com>
2026-04-15 21:04:22 +0200

    added initial main file for example

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { GameProvider } from "./ui/context/GameContext";
+import App from "./ui/components/App/App";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <GameProvider>
+      <App />
+    </GameProvider>
+  </StrictMode>,
+);
